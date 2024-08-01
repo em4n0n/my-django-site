aff064602975ffdb3e49ee64d0e4f190af3817e9
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-31 20:51:13 -0700

    Update title and slug variables from models

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,5 @@ from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
