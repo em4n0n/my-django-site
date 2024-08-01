dd093f2094d816cbccfa997845e4a367eebc8672
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-31 20:53:08 -0700

    Update summary and content variables
    TextField to not limit charfield for content

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,5 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
+    summary = models.CharField(max_length=300)
+    content = models.TextField()
