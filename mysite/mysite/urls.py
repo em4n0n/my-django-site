a305fbfc64d69cbce8181bfae46e23629a111593
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-01 20:12:48 -0700

    Route to the method in blog views

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from blog import views as blog_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'post/$', blog_views.post),
+    url(r'^admin/', admin.site.urls),
 ]
