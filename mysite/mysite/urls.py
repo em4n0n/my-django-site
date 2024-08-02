fc89977967c4a7e029285dd3f75f78bb2145051d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-01 20:14:05 -0700

    Load blog_views.index

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 from blog import views as blog_views
 
 urlpatterns = [
-    url(r'post/$', blog_views.post),
+    url(r'^post/$', blog_views.post),
+    url(r'^$', blog_views.index),
     url(r'^admin/', admin.site.urls),
 ]
